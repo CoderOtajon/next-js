7b8ae7e2048f197fe9054bbea99a1615631b7015
Coder <100829147+CoderOtajon@users.noreply.github.com>
2023-10-12 18:01:34 +0500

    some ui changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default function Home() {
   return (
-    <main>
+    <main className="bg-green-100 h-[100vh]">
       <Link href="/users">users</Link>
     </main>
   );
